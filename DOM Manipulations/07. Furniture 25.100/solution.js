6928816c7633b56eb8df59e061dd863ac42db1b0
thunder913 <andon_00@abv.bg>
2020-09-16 16:45:51 +0300

    DOM Manipulations Upload

--- DOM Manipulations/07. Furniture 25.100/solution.js
+++ DOM Manipulations/07. Furniture 25.100/solution.js
@@ -1,59 +1,68 @@
 function solve() {
-   let buttons = document.querySelectorAll('button');
-   for (const button of buttons) {
-     button.addEventListener('click', onClick);
-   }
+   
+  let inputArea = document.querySelector('textarea');
+  document.querySelector('button').addEventListener('click', onClickGenerate);
+ document.querySelectorAll('button')[1].addEventListener('click', onClickBuy);
 
-   function onClick(e)
-   {
-     let targetText = e.target.innerText;
-     if (targetText === 'Generate') {
-       let textItems = JSON.parse(document.querySelector('textarea').value);
-       document.querySelector('textarea').value = '';
-       for (const item of textItems) {
-         let table = document.querySelector('table');
-         let row = table.insertRow();
-
-         let img = document.createElement('img');
-         img.src = item.img;
-         row.insertCell().appendChild(img);
-       
-         let name = document.createElement('p');
-         name.innerText = item.name;
-         row.insertCell().appendChild(name);
-       
-         let price = document.createElement('p');
-         price.innerText = item.price;
-         row.insertCell().appendChild(price);
-
-         let decFactor = document.createElement('p');
-         decFactor.innerText = item.decFactor;
-         row.insertCell().appendChild(decFactor);
-
-         let mark = document.createElement('input');
-         mark.type = 'checkbox';
-         row.insertCell().appendChild(mark);
-        }
-      }else if(targetText === 'Buy')
-     {
-       let output = '';
-       let names = new Array();
-       let rows = document.querySelectorAll('tr');
-       let price = Number(0);
-       let decor = Number(0);
-       for (let i = 2; i < rows.length; i++) {
-         if (rows[i].cells[4].querySelector('input').checked) {
-           names.push(rows[i].cells[1].innerText);
-           price += Number(rows[i].cells[2].innerText);
-           decor += Number(rows[i].cells[3].innerText);
-         }
-       }
-
-       decor = decor / (rows.length-2);
-       output += `Bought furniture: ${names.join(', ')}
-Total price: ${price.toFixed(2)}
-Average decoration factor: ${decor}`;
-       document.querySelectorAll('textarea')[1].innerHTML = output;
+ function onClickBuy() {
+   let boughtProducts = new Array();
+   let price = 0;
+   let averageDecor = 0;
+   let inputs = document.querySelectorAll('tr');
+   for (let i = 2; i < inputs.length; i++) {
+     if (inputs[i]
+       .querySelector('input[type=checkbox]')
+       .checked) {
+         
+         let current = inputs[i].querySelectorAll('td');
+         boughtProducts.push(current[1].innerText);
+         price += Number(current[2].innerText)
+         averageDecor += Number(current[3].innerText);
      }
    }
+
+   let textBox = document.querySelectorAll('textarea')[1];
+   textBox.innerText = `Bought furniture: ${boughtProducts.join(', ')}\nTotal price: ${price.toFixed(2)}\nAverage decoration factor: ${averageDecor/boughtProducts.length}`;
+ }
+ 
+ function onClickGenerate() {
+   let obj = JSON.parse(inputArea.value);
+   let table = document.querySelector('table tbody');
+   for (let i = 0; i < obj.length; i++) {
+     let row = document.createElement('tr');
+
+     let picture = document.createElement('td');
+     let img = document.createElement('img');
+     img.setAttribute('src', obj[i].img);
+     picture.appendChild(img);
+     row.appendChild(picture);
+
+     let name = document.createElement('td');
+     let namePar = document.createElement('p');
+     namePar.innerText = obj[i].name;
+     name.appendChild(namePar);
+     row.appendChild(name);
+
+     let price = document.createElement('td');
+     let pricePar = document.createElement('p'); 
+     pricePar.innerText = obj[i].price;
+     price.appendChild(pricePar);
+     row.appendChild(price);
+
+     let decor = document.createElement('td');
+     let decorPar = document.createElement('p');
+     decorPar.innerText =obj[i].decFactor;
+     decor.appendChild(decorPar);
+     row.appendChild(decor);
+
+     let check = document.createElement('td');
+     let inputCheckBox = document.createElement('input');
+     inputCheckBox.setAttribute('type', 'checkbox');
+     check.appendChild(inputCheckBox);
+     row.appendChild(check);
+
+     table.appendChild(row);
+   }
+ }
+  
 }
